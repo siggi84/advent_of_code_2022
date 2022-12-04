80ce8a5bf7991ee88932e791592698c5d1b98b17
Sigurður Ægir Jónsson <sigurdur@noxmedical.com>
2022-12-04 12:38:02 +0000

    Day 4 in python

---
+++ 04/solution.py
@@ -0,0 +1,45 @@
+import pathlib
+from dataclasses import dataclass
+
+
+def part1(entries):
+    return sum(i1.contained(i2) or i2.contained(i1) for i1, i2 in entries)
+
+
+def part2(entries):
+    return sum(i1.overlap(i2) for i1, i2 in entries)
+
+
+@dataclass
+class Interval:
+    low: int
+    high: int
+
+    def overlaps(self, other):
+        return not (other.high < self.low or other.low > self.high)
+
+    def contains(self, other):
+        return self.low <= other.low and other.high <= self.high
+
+
+def main():
+    input = pathlib.Path("input.dat")
+
+    entries = []
+    for l in input.read_text().split("\n"):
+        if not l:
+            continue
+        pair = l.split(",")
+        iv1 = pair[0].split("-")
+        iv2 = pair[1].split("-")
+
+        interval1 = Interval(int(iv1[0]), int(iv1[1]))
+        interval2 = Interval(int(iv2[0]), int(iv2[1]))
+        entries.append((interval1, interval2))
+
+    print(part1(entries))
+    print(part2(entries))
+
+
+if __name__ == "__main__":
+    main()
